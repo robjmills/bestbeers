ff81f1b462107e1e79b18c3e43be24eafdb2b9d3
Rob Mills <robjmills@gmail.com>
2014-02-21 16:30:26 +0000

    Fuck you pagodabox

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -27,7 +27,7 @@ $app = new Illuminate\Foundation\Application;
 $env = $app->detectEnvironment(array(
 
 	'local'	=> array('Robs-rMBP.local'),
-	'pagoda' => array('http://beerhere.gopagoda.com/')
+	'pagoda' => array('http://beerhere.gopagoda.com')
 
 ));
 
