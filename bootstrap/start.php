dd178d6a10242808a238c7a08f5638e9bd1ba60b
Rob Mills <robjmills@gmail.com>
2014-02-21 16:23:32 +0000

    detect the pagoda environment

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('Robs-rMBP.local'),
+	'local'	=> array('Robs-rMBP.local'),
+	'pagoda' => array('http://beerhere.gopagoda.com/')
 
 ));
 
