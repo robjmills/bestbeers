c742b8c419aef41c0d54f9eb491f3e95055cf9a9
Rob Mills <robjmills@gmail.com>
2015-02-21 12:46:58 +0000

    changing the site name

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Beershere</title>
+        <title>Bestbeers.in</title>
         <meta name="viewport" content="width=device-width">
         <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
         <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
@@ -15,13 +15,7 @@
     <div class="navbar navbar-default navbar-static-top" role="navigation">
         <div class="container">
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Beerhere</a>
+                <a class="navbar-brand" href="#">Bestbeers.in</a>
             </div>
 
         </div>
