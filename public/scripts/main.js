383f8697b74e506ad4a36ded4b520085c84fc33b
Rob Mills <robjmills@gmail.com>
2015-02-28 09:35:02 +0000

    initial work on ajax push state  #1

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,58 +1,85 @@
-'use strict';
-
-function error(err) {
-    console.warn('ERROR(' + err.code + '): ' + err.message);
-};
-
-function success(pos) {
-
-    var crd = pos.coords;
-
-    $.ajax({
-        url: "/location?lat="+crd.latitude+"&long="+crd.longitude
-    }).done(function ( data ) {
-
-        var locations = jQuery.parseJSON( data );
-        var venues = locations.response.venues;
-
-        var template = $.templates("#venuetmpl");
-        template.link("#venues", venues);
-        $('#info').hide();
-        $('#venues').show();
-
-        $('#venues a').bind('click',function(event){
-            event.preventDefault();
-            $('#chosen').html($(this).text()).show();
-            $('#msg').html('finding their best beers...');
-            $('#info').show();
-            $('#venues').hide()
-            $(this).addClass('selected');
-            $.ajax({
-                url: this.href
-            }).done(function ( data ) {
-                var btemplate = $.templates("#beertmpl");
-                btemplate.link("#beers", data);
-                $('#beers').show();
-                $('#info').hide();
-                $('#back').show();
-            });
-        });
-    });
-};
-
 (function(){
 
+    'use strict';
+
+    // geolocation options
     var options = {
         enableHighAccuracy: true,
         timeout: 5000,
         maximumAge: 0
     };
 
+    // detect user location
     navigator.geolocation.getCurrentPosition(success, error, options);
 
+    // back button controls
     $('#back').click(function(){
         $('#venues').show();
         $('#beers, #chosen, #back').hide();
     });
+
+    // listen for user clicking back/forward
+    window.addEventListener("popstate", function(e) {
+        
+        // load location fragment - what about rest of the page?
+        if(location.pathname === '/location'){
+            loadLocation(location.pathname+location.search);
+        }
+    });
+
+    function error(err) {
+        console.warn('ERROR(' + err.code + '): ' + err.message);
+    }
+
+    function success(pos) {
+        var crd = pos.coords;
+        var location = "/location?lat="+crd.latitude+"&long="+crd.longitude;
+        loadLocation(location);
+    }
+
+    function loadLocation(location){
+        $.ajax({
+            url: location
+        }).done(function ( data ) {
+
+            var locations = jQuery.parseJSON( data );
+            var venues = locations.response.venues;
+
+            var template = $.templates("#venuetmpl");
+            template.link("#venues", venues);
+            $('#info').hide();
+            $('#venues').show();
+
+            // set URL to location
+            history.pushState(null, null, location);
+
+            $('#venues a').bind('click',function(event){
+                event.preventDefault();
+                $('#chosen').html($(this).text()).show();
+                $('#msg').html('finding their best beers...');
+                $('#info').show();
+                $('#venues').hide();
+                $(this).addClass('selected');
+                var venue = this.href;
+                loadVenue(venue);
+            });
+        });
+    }
+
+    function loadVenue(venue){
+
+        $.ajax({
+            url: venue 
+        }).done(function ( data ) {
+            var btemplate = $.templates("#beertmpl");
+            btemplate.link("#beers", data);
+            $('#beers').show();
+            $('#info').hide();
+            $('#back').show();
+
+            // set URL to location
+            history.pushState(null, null, venue);
+        });
+    }
 
 })();
